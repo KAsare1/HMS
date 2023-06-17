4d6e377b1a847e9420fcd93fa927bcc806b5401f
KAsare1 <asarek977@gmail.com>
2023-06-17 01:48:04 +0000

    url modifications

--- HMS/accounts/urls.py
+++ HMS/accounts/urls.py
@@ -5,6 +5,6 @@ from django.contrib.auth.urls import views
 from . import views
 
 urlpatterns = [
-    path("login/", views.login_request, name="login"),
+    path("", views.login_request, name="login"),
     path("register/", views.register_request, name="register"),
 ]
