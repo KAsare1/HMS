57866c09fcea7edbc4a607e2e0085c5d1fec5603
KAsare1 <asarek977@gmail.com>
2023-06-19 01:06:27 +0000

    nurses model

--- HMS/dashboards/models.py
+++ HMS/dashboards/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 # Create your models here.
 class NursesPage(models.Model):
-    Height = models.FloatField(max_length=5)
+    Blood_Pressure = models.CharField(max_length=10)
+    Weight = models.CharField(max_length=10)
+    Temperature = models.CharField(max_length=10)
+    Pulse_rate = models.CharField(max_length=10)
+
+class Doctorpage(models.Model):
+    
